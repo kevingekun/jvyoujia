0f3b83c926a2b3f0fbc14a4cb7d945abfc6c6f06
gkk <1021813835@qq.com>
2016-12-25 12:32:41 +0800

    the start

---
+++ jvyoujia_git/Application/Common/Conf/wechat.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'APP_ID' => 'wx79daf5818e31c4aa',
+  'APP_SECRET' => '284552ca38e749905768fe51bbcd6845',
+  'APP_TOKEN' => 'ab21e6c443ef36bd0de2e353c809aefe',
+  'APP_KEY' => 'rS2zuywxsLIRp4iVsq1RKY3vjB1SImJst6qH89noPzp',
+  'MCH_id' => '1253660801',
+  'KEY' => '530943e06e4d1df94ff887a20affc36c',
+  'NOTIFY_URL' => 'http://jvyoujia.startsrun.com/Mall/PayCenter/notify.html',
+);
